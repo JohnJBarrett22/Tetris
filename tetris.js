83a4e07a1f5027722dcbcdd502dc27838724ed46
John Barrett <johnjbarrett22@gmail.com>
2019-01-12 12:35:09 -0800

    Created draw function and established game board

--- tetris.js
+++ tetris.js
@@ -1,2 +1,23 @@
 const cvs = document.getElementById("tetris");
 const ctx = cvs.getContext("2d");
+
+const row = 20;
+const col = column = 10;
+const sq = squareSize = 20;
+const vacant = "white";
+
+function draw(x, y, color){
+    ctx.fillStyle = color;
+    ctx.fillRect(0*sq,0*sq,sq,sq);
+
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(0*sq,0*sq,sq,sq);
+}
+
+let board = [];
+for(r = 0; r < row; r++){
+    board[r] = [];
+    for(c = 0; c < col; c++){
+        board[r][c] = vacant;
+    }
+}
